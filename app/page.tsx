5e7383f2b8c09434870afc381f8352b26941f3cc
MoaazAllaEden <moaz.dev@yahoo.com>
2024-10-06 00:03:58 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import {  Main } from "@/components/Layouts";
 import { InterconnectionsList, Systems } from "./shared";
-import { CtaClimateChange } from "./components/CtaClimateChange";
 import { Hero } from "./components/Hero";
 import { AllSystems } from "./components/AllSystems";
 import { Interconnections } from "./components/Interconnections";
@@ -10,7 +9,7 @@ export default function Home() {
     return (
         <Main className="flex flex-col gap-40 max-lg:!gap-10 bg-white">
             <Hero className="max-xl:!px-6" />
-            <CtaClimateChange className="max-xl:!px-8 max-xl:!gap-10 max-md:flex-col" />
+            {/* <CtaClimateChange className="max-xl:!px-8 max-xl:!gap-10 max-md:flex-col" /> */}
             <CtaSandysIdea />
             {/* <Section.CtaNasaData className="max-xl:!px-8 max-xl:!gap-10 max-md:flex-col" /> */}
             <AllSystems {...{ Systems }} className="max-lg:!px-7" />
